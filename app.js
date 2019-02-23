f467c284977a77b6b6f8a84478e7668459369daa
komura <h.komura2@gmail.com>
2019-02-23 20:07:13 -0300

    Change approach to use orders instead of list values

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
+const orders = require("./orders");
 const express = require("express");
+
+const fromZeroToTwenty = orders.fromZeroToTwenty;
+const dozens = orders.dozens;
+const hundreds = orders.hundreds;
 const app = express();
 const port = 3000;
 
@@ -6,23 +11,64 @@ const port = 3000;
  * range: [ -99999, 99999 ]
  * six characters, including the signal
  * */
-
+app.get('/favicon.ico', (req, res) => res.status(204));
 app.get("/:id", (req, res) => {
-  // make a copy of value, without modifying it directly
-  const value = req.params.id; 
-  const copy = [...value];
-  const orders = [
-    { 0: "", 1: "um", 2: "dois", 3: "tres", 4: "quatro" },
-    { 0: "", 1: "dez", 2: "vinte", 3: "trinta", 4: "quarenta" },
-    { 0: "", 1: "cem", 2: "duzentos", 3: "trezentos", 4: "quatrocentos" }
-  ];
+  const value = parseInt(req.params.id);
+  if (Math.abs(value) > 99999) {
+    return res.send("id must be at range of [-99999, 99999]");
+  }
 
-  const slicedArray = Array.from(copy);
-  const element = parseInt(slicedArray[0]);
+  function toWords(number) {
+    let words;
+    let num = parseInt(number, 10);
+    words = generateWords(num)
+    return res.send(`{"id": ${words}}`);
+  };
+  
+  function generateWords(number) {
+    let remainder, word, words = arguments[1];
+    if (number === 0) {
+      return !words ? "zero" : words.join(" ").replace(/,$/, "");
+    }
+    if (!words) {
+      words = [];
+    }
+    if (number < 0) {
+      words.push("menos");
+      number = Math.abs(number);
+    }
+    if (number < 20) {
+      remainder = 0;
+      word = fromZeroToTwenty[number]
+    } else if (number < 100) {
+      remainder = number % 10;
+      word = dozens[Math.floor(number / 10)];
+      if (remainder) {
+        word += " e " + fromZeroToTwenty[remainder]
+        remainder = 0;
+      }
+    } else if (number <= 1000) {
+      remainder = number % 100;
+      word = hundreds[Math.floor(number / 100) - 1];
+      if (remainder) {
+        unit = remainder % 10;
+        word += " e " + dozens[Math.floor(remainder / 10)]
+        remainder = 0;
+        if (unit) {
+          word += " e " + fromZeroToTwenty[unit]
+        }
+      }
+    } else if (number < 100000) {
+      remainder = number % 1000;
+      word = `${generateWords(Math.floor(number / 1000))} mil`
+    } 
 
-  const mapElementToOrder = slicedArray.map((el, index) => orders[slicedArray.length - index - 1][slicedArray[index]]);
-
-  return res.send(`{ id: ${mapElementToOrder} }`);
+    words.push(word)
+    return generateWords(remainder, words);
+  }
+  
+  toWords(value);
+  console.log('toWords(value)', toWords(value))
 });
 
 app.listen(port, () => console.log(`You can now visit the app at http://localhost:${port}`)); 
---
+++ orders.js
@@ -0,0 +1,10 @@
+const fromZeroToTwenty = ["zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezesete", "dezoito", "dezenove"];
+
+const dozens = ["zero", "dez", "vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa", "cem"];
+
+const hundreds = ["cento", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos"];
+module.exports = {
+  fromZeroToTwenty,
+  dozens,
+  hundreds,
+};
